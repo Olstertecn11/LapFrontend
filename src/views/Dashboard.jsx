cb907ee5cd3345bab76a5ae922174ab4706a1841
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-25 13:52:29 -0600

    creating dashboard xd

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,19 @@
+
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-10">
+            <h2>Dashboard</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Dashboard;
