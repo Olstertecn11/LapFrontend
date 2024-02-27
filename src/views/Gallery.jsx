be6962c7d30fc3e155bb67e7fd45c48ece0409f4
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-27 17:23:02 -0600

    Gallery is the events view

--- src/views/Gallery.jsx
+++ src/views/Gallery.jsx
@@ -1,12 +1,42 @@
+import './styles/Gallery.css';
+import ActivityService from './../services/activities/ActivityService.js';
+import { useEffect, useState } from 'react';
 
-const Gallery = () => {
+const GalleryEvents = () => {
+
+  const [activities, setActivities] = useState([])
+
+
+
+  const fetchData = async () => {
+    const response = await ActivityService.getAll();
+    setActivities(response);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h2>My Gallery</h2>
+      <div className="row m-4">
+        {
+          activities.map(item => (
+            <div className="col-md-4">
+              <div className="card gallery-card" >
+                <div className="card-body">
+                  <h2>{item.act_name}</h2>
+                </div>
+              </div>
+            </div>
+
+          ))
+        }
+      </div>
     </div>
 
   )
 }
 
 
-export default Gallery;
+export default GalleryEvents;
