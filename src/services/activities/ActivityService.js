ec011ec9c679c3e2dff6e9e8e6eaab20093c9037
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-27 18:39:17 -0600

    get images from activity

--- src/services/activities/ActivityService.js
+++ src/services/activities/ActivityService.js
@@ -1,17 +1,33 @@
 import axios from 'axios';
 import { getApiUrl } from './../../config/config';
 
-const ActivityService = {
-  getAll: async () => {
-    const apiUrl = getApiUrl('/activities')
+const apiUrl = getApiUrl('/activities')
 
+const ActivityService = {
+
+  getAll: async () => {
     return axios.get(apiUrl, {
       headers: {
         "Content-Type": "application/json",
         "Authorization": "Bearer <token>" // Aquí deberías incluir tu token de autorización si lo tienes
       }
     }).then(res => res.data).catch(err => console.log(err));
+  },
+  getImagesFromActivity: async (id) => {
+    const url = getApiUrl(`/activities/images`);
+    return axios.get(url, {
+      params: {
+        idActivity: id
+      },
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer <token>" // Aquí deberías incluir tu token de autorización si lo tienes
+      }
+    })
+      .then(res => res.data)
+      .catch(err => console.log(err));
   }
+
 }
 
 export default ActivityService;
