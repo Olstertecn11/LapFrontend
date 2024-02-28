e16d2600c264f29844a3e1919e72b0816382f407
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-27 18:38:55 -0600

    adding new routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import Dashboard from './views/Dashboard';
 import Container from './layouts/Container';
 import Navbar from "./layouts/Navbar";
 import GalleryEvents from "./views/Gallery";
+import GalleryImages from "./views/GalleryImages";
 import Layout from "./layouts/Layout";
 
 function App() {
 
   return (
     <>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element=<Login /> />
           <Route path="/Bienvenida" element=<WelcomePage /> />
           <Route path="/Dashboard" element={<Layout Component={Dashboard} />} />
-          <Route path="/Galeria" element={<Layout Component={GalleryEvents} />} />
+          <Route path="/Galeria" element={<Layout Component={GalleryEvents} />} />;
+          <Route path="/GaleriaImagenes/:id" element={<Layout Component={GalleryImages} />} />
         </Routes>
       </BrowserRouter>
     </>
