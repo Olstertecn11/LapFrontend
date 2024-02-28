a67c6f77b50e2ba6283b29211bed2d432a454bc1
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-27 23:53:04 -0600

    new dashboard design

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,10 +1,22 @@
-
-
+import GridCardList from "../components/GridCardList";
+import { Stack, Heading, Text, Container } from "@chakra-ui/react";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h2>Hola mundo</h2>
+    <div style={{ background: '#b6d0d6' }}>
+      {
+        /*
+              <Stack spacing={4} as={Container} maxW={'3xl'} textAlign={'center'} mt={'4'}>
+                <Heading fontSize={{ base: '2xl', sm: '4xl' }} fontWeight={'bold'}>
+                  Bienvenido al Sistema
+                </Heading>
+                <Text color={'gray.600'} fontSize={{ base: 'sm', sm: 'lg' }}>
+                  Esta es una plataforma interactiva para poder administrar los recursos academicos del Liceo Adventista el progreso
+                </Text>
+              </Stack>
+                */
+      }
+      <GridCardList />
     </div>
   )
 }
