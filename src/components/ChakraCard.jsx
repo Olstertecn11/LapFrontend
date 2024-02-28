2d0f717795ca6119f6a0b405d86e23a52c1d7dbd
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-27 23:53:45 -0600

    cards tamplate

---
+++ src/components/ChakraCard.jsx
@@ -0,0 +1,56 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+
+const ChakraCard = ({ heading, description, icon, href, callback }) => {
+
+  const history = useNavigate();
+
+  const handleRedirect = (link) => {
+    history(link)
+    callback();
+  }
+  return (
+    <Box
+      boxShadow={'2xl'}
+      background={'white'}
+      maxW={{ base: 'full', md: '275px' }}
+      w={'full'}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={5}>
+      <Stack align={'start'} spacing={2}>
+        <Flex
+          w={16}
+          h={16}
+          align={'center'}
+          justify={'center'} color={'white'}
+          rounded={'full'}
+          bg={useColorModeValue('gray.100', 'gray.700')}>
+          {icon}
+        </Flex>
+        <Box mt={2}>
+          <Heading size="md">{heading}</Heading>
+          <Text mt={1} fontSize={'sm'}>
+            {description}
+          </Text>
+        </Box>
+        <Button variant={'ghost'} colorScheme={'blue'} size={'sm'} onClick={() => handleRedirect(href)} >
+          Visitar
+        </Button>
+      </Stack>
+    </Box>
+  )
+}
+
+export default ChakraCard;
