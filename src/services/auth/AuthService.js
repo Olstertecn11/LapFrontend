e4a5521ccc92ca122d3fce02c82fca3c5f5cf469
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 13:27:29 -0600

    logout function done

--- src/services/auth/AuthService.js
+++ src/services/auth/AuthService.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import { getApiUrl } from './../../config/config';
 
+const apiUrl = getApiUrl('/auth')
 const AuthService = {
+
   login: async ({ username, password }) => {
-    const apiUrl = getApiUrl('/auth')
 
     return axios.post(apiUrl, { username, password }, {
       headers: {
@@ -11,7 +12,18 @@ const AuthService = {
         "Authorization": "Bearer <token>" // Aquí deberías incluir tu token de autorización si lo tienes
       }
     }).then(res => res.data).catch(err => console.log(err));
+  },
+  logout: async (idUser) => {
+    try {
+      const response = await axios.delete(apiUrl + `&id=${idUser}`);
+      return response.data;
+    } catch (error) {
+      console.error('Error en la solicitud DELETE:', error);
+      throw error; // Re-lanzar el error para que pueda ser manejado fuera de esta función
+    }
   }
+
+
 }
 
 export default AuthService;
