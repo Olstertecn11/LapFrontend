a171da10b40fe19e25cddef96a5210ed5ecd5fc2
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 21:48:47 -0600

    verifying session

---
+++ src/hooks/useSession.jsx
@@ -0,0 +1,38 @@
+
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import authService from "../services/auth/AuthService";
+
+const useSession = () => {
+  const history = useNavigate();
+
+  const verifySession = async () => {
+    try {
+      const userObject = JSON.parse(localStorage.getItem('session'));
+      if (!userObject) {
+        history('/');
+        return;
+      }
+
+      const { idUsr, token } = userObject;
+      const response = await authService.hasSession(idUsr, token);
+      if (!response.exist) {
+        history('/');
+      }
+      else {
+        console.log('is logged');
+      }
+    } catch (error) {
+      console.error('Error al verificar la sesión:', error);
+    }
+  };
+
+  useEffect(() => {
+    verifySession();
+  }, []);
+
+  return verifySession;
+};
+
+export default useSession;
+
