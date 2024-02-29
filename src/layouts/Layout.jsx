4879b07912e1e3946104fe8446424a1963139d59
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 21:49:28 -0600

    using useSession on Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./Navbar";
 import { ChakraProvider } from "@chakra-ui/react";
+import useSession from "../hooks/useSession";
 
 
 
 
 const Layout = ({ Component }) => {
+
+  const MySession = useSession();
+
   return (
     <ChakraProvider>
       <Navbar />
