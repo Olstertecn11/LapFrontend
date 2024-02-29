0ef83f27c18150b8a50be4eb009975fcf37b0121
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 21:49:11 -0600

    sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Button
+} from '@chakra-ui/react'
+
+const Sidebar = ({ Component, title, isOpen, onOpen, onClose, btnRef, updateData }) => {
+
+  return (
+    <div>
+
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>{title}</DrawerHeader>
+
+          <DrawerBody>
+            {
+              Component && (
+                <Component updateData={updateData} event={onOpen} />
+              )
+            }
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' colorScheme={'red'} mr={0} onClick={onClose}>
+              Cancelar
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  )
+}
+
+export default Sidebar;
