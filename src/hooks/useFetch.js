2f2121f3da295958d0b56ccf625a63729006b78a
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 21:48:41 -0600

    use Fetch working

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,7 @@
+
 import { useState, useEffect } from 'react';
 
-export const useFetch = (myService) => {
+const useFetch = (myService, params) => {
 
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
@@ -11,18 +12,35 @@ export const useFetch = (myService) => {
       setIsPending(true);
       try {
         const response = await myService();
-        if (response.ok) throw Error(response.statusText);
-        setIsPending(false)
         setData(response);
+        setIsPending(false);
         setError(null);
-      }
-      catch (error) {
+      } catch (error) {
         setError(error);
         setIsPending(false);
       }
-    }
+    };
+
     fetchData();
 
   }, []);
-  return { data, isPending, error }
-}
+
+  const updateData = async () => {
+    try {
+      setIsPending(true);
+      const response = await myService();
+      const responseData = await response;
+      setData(responseData);
+      setIsPending(false);
+      setError(null);
+    } catch (error) {
+      setError(error);
+      setIsPending(false);
+    }
+  };
+
+  return { data, isPending, error, updateData };
+};
+
+export default useFetch;
+
