df9b12492532550929846af2113da91dc29dc799
Oliver Tzunun <49626428+Olstertecn11@users.noreply.github.com>
2024-02-28 21:49:21 -0600

    removing container

--- src/layouts/Container.jsx
+++ src/layouts/Container.jsx
@@ -1,4 +1,3 @@
-import SideBar from './Sidebar';
 
 
 const Container = ({ Component }) => {
@@ -6,7 +5,6 @@ const Container = ({ Component }) => {
     <div className="container-fluid h-100">
       <div className="row justify-content-center h-100">
         <div className="col-4 hidden-md-down">
-          <SideBar />
         </div>
         <div className="col-10 col-sm-10 col-md-10 col-lg-8 col-xl-8">
           <Component />
