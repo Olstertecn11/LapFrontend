d6c0c9d0c3ebfb786fcd2527b7c0482b31abdb10
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-03-14 16:08:14 -0600

    admin dashboard done

--- src/views/AdminDashboard.jsx
+++ src/views/AdminDashboard.jsx
@@ -1,36 +1,20 @@
-import ControlBox from "../components/ControlsBox";
-import { useDisclosure } from "@chakra-ui/react";
-import { useState, useRef, useEffect } from 'react';
-import { Text, useColorMode, useColorModeValue } from "@chakra-ui/react";
+
+import AdminSection from "../layouts/AdminSection";
+import UserService from '../services/user/UserService';
+import NewUser from '../layouts/NewUser';
 
 const AdminDashboard = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef()
+
+
+
+
   return (
     <div>
       <div className="container pt-4">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card">
-              <div className="pl-4 pt-4">
-                <Text
-                  className="text-center text-white"
-                  ml={4}
-                  textTransform="uppercase"
-                  width={'60%'}
-                  bg={useColorModeValue('blue.400', 'blue.700')}
-                  px={3}
-                  py={1}
-                  fontSize="sm"
-                  fontWeight="600"
-                  rounded="xl">
-                  Gestion de maestros
-                </Text>
-              </div>
-              <ControlBox refArr={[btnRef]} handleArr={[onOpen]} />
-            </div>
-          </div>
-        </div>
+        <AdminSection title='Gestion de Maestros' ServiceDelete={UserService.delete} ServiceGet={UserService.getAll} titleNew='Nuevo Usuario' ComponentNew={NewUser} />
+      </div>
+      <div className="container pt-4 mt-4 mb-4">
+        <AdminSection title='Gestion de Cursos' ServiceDelete={UserService.delete} ServiceGet={UserService.getAll} titleNew='Nuevo Usuario' ComponentNew={NewUser} />
       </div>
     </div>
   )
