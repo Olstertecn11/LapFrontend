a908e3d579ffe2ea3aa43e05310e2f1a93c15035
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-03-14 16:58:29 -0600

    fix layouting

--- src/views/AdminDashboard.jsx
+++ src/views/AdminDashboard.jsx
@@ -1,7 +1,9 @@
 
 import AdminSection from "../layouts/AdminSection";
 import UserService from '../services/user/UserService';
+import SubjectService from "../services/subject/SubjectService";
 import NewUser from '../layouts/NewUser';
+import NewSubject from '../layouts/NewSubject';
 
 const AdminDashboard = () => {
 
@@ -10,12 +12,11 @@ const AdminDashboard = () => {
 
   return (
     <div>
-      <div className="container pt-4">
+      <div className="container pt-4 mb-4">
         <AdminSection title='Gestion de Maestros' ServiceDelete={UserService.delete} ServiceGet={UserService.getAll} titleNew='Nuevo Usuario' ComponentNew={NewUser} />
+        <AdminSection margin={'8vw'} title='Gestion de Asignaturas' ServiceDelete={SubjectService.delete} ServiceGet={SubjectService.getAll} titleNew='Nueva Asignatura' ComponentNew={NewSubject} />
       </div>
-      <div className="container pt-4 mt-4 mb-4">
-        <AdminSection title='Gestion de Cursos' ServiceDelete={UserService.delete} ServiceGet={UserService.getAll} titleNew='Nuevo Usuario' ComponentNew={NewUser} />
-      </div>
+
     </div>
   )
 }
