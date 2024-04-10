3f46ba9499defd779de7e5c320344b2c0dacb5dc
Oliver Tzunun <otzunund@miumg.edu.gt>
2024-04-09 23:26:45 -0600

    for deploy

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
-export const getApiUrl = path => `http://localhost:3000${path}?api_key=${API_KEY}`;
-// export const getApiUrl = path => `https://lap-backend-rflw.vercel.app${path}?api_key=${API_KEY}`;
+// export const getApiUrl = path => `http://localhost:3000${path}?api_key=${API_KEY}`;
+export const getApiUrl = path => `https://lap-backend-rflw.vercel.app${path}?api_key=${API_KEY}`;
